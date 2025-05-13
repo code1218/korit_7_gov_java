eeecdc8e3e88652f05f7b21130c0b0261191cbf0
code1218 <skjil1218@gmail.com>
2025-05-13 17:58:24 +0900

    25.05.13 수업

--- src/main/java/oop/BasicUserService.java
+++ src/main/java/oop/BasicUserService.java
@@ -3,8 +3,6 @@ package oop;
 public class BasicUserService {
 
     private UserRepository userRepository;
-//    private CustomUserRepository userRepository2 = (CustomUserRepository) userRepository;
-//    private BasicUserRepository userRepository2 = (BasicUserRepository) userRepository;
 
     public BasicUserService(UserRepository userRepository) {
         this.userRepository = userRepository;
--- src/main/java/oop/Main.java
+++ src/main/java/oop/Main.java
@@ -3,8 +3,10 @@ package oop;
 public class Main {
 
     public static void main(String[] args) {
-//        UserRepository userRepository = new BasicUserRepository();
-        BasicUserService userService = new BasicUserService(new CustomUserRepository());
+        BasicUserRepository basicUserRepository = new BasicUserRepository();
+        CustomUserRepository customUserRepository = new CustomUserRepository();
+
+        BasicUserService userService = new BasicUserService(basicUserRepository);
 
         userService.addUser();
     }
